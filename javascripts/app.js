e736fe76b4325ce95cac066b1ee08775b8267e5a
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-15 10:37:21 -0600

    you can not move forward unless you select a weapon

--- javascripts/app.js
+++ javascripts/app.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
         moveAlong = ($("#player-name").val() !== "");
         break;
       case "card--weapon":
-        moveAlong = ($("#player-name").val() !== "");
+        moveAlong = (currentPlayer !== undefined);
         break;
       case "card--spell":
         moveAlong = ($("#player-name").val() !== "");
